e4e320a7ab9e535f72923e14d9bf57887073947b
Artur <artur.helmanau@coinweb.io>
2022-10-11 22:21:14 +0300

    feat: server

--- msg/src/lib.rs
+++ msg/src/lib.rs
@@ -1,14 +1,245 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use either::Either;
+pub use rsa::{RsaPrivateKey, RsaPublicKey};
+
+use aes::{
+    cipher::{BlockDecryptMut, BlockEncryptMut, KeyInit},
+    Aes256,
+};
+use generic_array::GenericArray;
+use rand::{CryptoRng, RngCore};
+use rsa::{errors::Result as RsaResult, PaddingScheme, PublicKey};
+use serde::{Deserialize, Serialize};
+use serde_encrypt::{serialize::impls::BincodeSerializer, traits::SerdeEncryptSharedKey};
+use typenum::consts::U16;
+
+pub type AesKey = aes::cipher::Key<Aes256>;
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash, Serialize, Deserialize)]
+pub struct GreetRequest(pub RsaPublicKey);
+
+impl GreetRequest {
+    pub fn to_response<R: CryptoRng + RngCore>(
+        self,
+        rng: &mut R,
+        key: &AesKey,
+    ) -> RsaResult<GreetResponse> {
+        self.0
+            .encrypt(rng, PaddingScheme::new_pkcs1v15_encrypt(), key)
+            .map(|bytes| (self, bytes))
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub type EncryptedAesKey = Vec<u8>;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub type GreetResponse = (GreetRequest, EncryptedAesKey);
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash, Serialize, Deserialize)]
+pub struct EncryptedData {
+    pub content: Vec<GenericArray<u8, U16>>,
+    pub zero_suffix_len: usize,
+}
+
+impl EncryptedData {
+    pub fn encrypt<T: Serialize>(x: &T, key: &AesKey) -> serde_cbor::Result<Self> {
+        let bytes = serde_cbor::to_vec(x)?;
+        let zero_suffix_len = bytes.len() % 16;
+        let mut content: Vec<_> = bytes
+            .chunks(16)
+            .map(|chunk| {
+                if chunk.len() == 16 {
+                    GenericArray::from_slice(chunk).clone()
+                } else {
+                    let mut array = [0; 16];
+                    array[0..chunk.len()].copy_from_slice(chunk);
+                    GenericArray::from_slice(&array).clone()
+                }
+            })
+            .collect();
+        let mut cipher = Aes256::new(key);
+        cipher.encrypt_blocks_mut(&mut content);
+        Ok(Self {
+            content,
+            zero_suffix_len,
+        })
+    }
+
+    pub fn decrypt<T: for<'de> Deserialize<'de>>(mut self, key: &AesKey) -> serde_cbor::Result<T> {
+        let mut cipher = Aes256::new(key);
+        cipher.decrypt_blocks_mut(&mut self.content);
+        let mut bytes = Vec::with_capacity(self.content.len());
+        for chunk in &self.content[..self.content.len().saturating_sub(1)] {
+            bytes.extend_from_slice(chunk);
+        }
+        if let Some(last) = self.content.last() {
+            bytes.extend_from_slice(&last[..16usize.saturating_sub(self.zero_suffix_len)]);
+        }
+        serde_cbor::from_slice(&bytes)
+    }
+}
+
+impl SerdeEncryptSharedKey for EncryptedData {
+    type S = BincodeSerializer<Self>;
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash, Serialize, Deserialize)]
+pub struct Paste {
+    pub name: String,
+    pub content: String,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash, Serialize, Deserialize)]
+pub struct EncryptedPaste {
+    pub name: EncryptedData,
+    pub content: EncryptedData,
+}
+
+impl EncryptedPaste {
+    pub fn decrypt_name(&self, key: &AesKey) -> serde_cbor::Result<String> {
+        self.name.clone().decrypt(key)
+    }
+
+    pub fn decrypt_content(&self, key: &AesKey) -> serde_cbor::Result<String> {
+        self.content.clone().decrypt(key)
+    }
+
+    pub fn decrypt(&self, key: &AesKey) -> serde_cbor::Result<Paste> {
+        Ok(Paste {
+            name: self.decrypt_name(key)?,
+            content: self.decrypt_content(key)?,
+        })
+    }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash, Serialize, Deserialize)]
+pub enum ActionRequest {
+    Get { name: String },
+    Remove { name: String },
+    Mut(Paste),
+    New(Paste),
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash, Serialize, Deserialize)]
+pub enum EncryptedActionRequest {
+    New(EncryptedPaste),
+    Mut(EncryptedPaste),
+    Get { name: EncryptedData },
+    Remove { name: EncryptedData },
+}
+
+impl EncryptedActionRequest {
+    pub fn decrypt(self, key: &AesKey) -> serde_cbor::Result<ActionRequest> {
+        Ok(match self {
+            EncryptedActionRequest::New(paste) => ActionRequest::New(paste.decrypt(key)?),
+            EncryptedActionRequest::Mut(paste) => ActionRequest::Mut(paste.decrypt(key)?),
+            EncryptedActionRequest::Get { name } => ActionRequest::Get {
+                name: name.decrypt(key)?,
+            },
+            EncryptedActionRequest::Remove { name } => ActionRequest::Remove {
+                name: name.decrypt(key)?,
+            },
+        })
+    }
+
+    pub fn to_response(
+        self,
+        payload: Either<Option<EncryptedPaste>, EncryptedAesKey>,
+    ) -> EncryptedActionResponse {
+        (self, payload)
+    }
+
+    pub fn name(&self) -> &EncryptedData {
+        match self {
+            EncryptedActionRequest::New(EncryptedPaste { name, .. }) => name,
+            EncryptedActionRequest::Mut(EncryptedPaste { name, .. }) => name,
+            EncryptedActionRequest::Get { name } => name,
+            EncryptedActionRequest::Remove { name } => name,
+        }
+    }
+
+    pub fn as_get(&self) -> Option<&EncryptedData> {
+        if let Self::Get { name } = self {
+            Some(name)
+        } else {
+            None
+        }
+    }
+}
+
+pub type EncryptedActionResponse = (
+    EncryptedActionRequest,
+    Either<Option<EncryptedPaste>, EncryptedAesKey>,
+);
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash, Serialize, Deserialize)]
+pub enum Msg {
+    GreetRequest(GreetRequest),
+    GreetResponse(GreetResponse),
+    EncryptedActionRequest(EncryptedActionRequest),
+    EncryptedActionResponse(EncryptedActionResponse),
+}
+
+impl Msg {
+    pub fn as_greet_request(&self) -> Option<&GreetRequest> {
+        if let Self::GreetRequest(request) = self {
+            Some(request)
+        } else {
+            None
+        }
+    }
+
+    pub fn as_greet_response(&self) -> Option<&GreetResponse> {
+        if let Self::GreetResponse(response) = self {
+            Some(response)
+        } else {
+            None
+        }
+    }
+
+    pub fn as_encrypted_action_request(&self) -> Option<&EncryptedActionRequest> {
+        if let Self::EncryptedActionRequest(request) = self {
+            Some(request)
+        } else {
+            None
+        }
+    }
+
+    pub fn as_encrypted_action_response(&self) -> Option<&EncryptedActionResponse> {
+        if let Self::EncryptedActionResponse(response) = self {
+            Some(response)
+        } else {
+            None
+        }
+    }
+
+    pub fn greet_request(self) -> Option<GreetRequest> {
+        if let Self::GreetRequest(request) = self {
+            Some(request)
+        } else {
+            None
+        }
+    }
+
+    pub fn greet_response(self) -> Option<GreetResponse> {
+        if let Self::GreetResponse(response) = self {
+            Some(response)
+        } else {
+            None
+        }
+    }
+
+    pub fn encrypted_action_request(self) -> Option<EncryptedActionRequest> {
+        if let Self::EncryptedActionRequest(request) = self {
+            Some(request)
+        } else {
+            None
+        }
+    }
+
+    pub fn encrypted_action_response(self) -> Option<EncryptedActionResponse> {
+        if let Self::EncryptedActionResponse(response) = self {
+            Some(response)
+        } else {
+            None
+        }
     }
 }
--- pastebin/src/lib.rs
+++ pastebin/src/lib.rs
@@ -1,14 +1,147 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use curl::easy as curl;
+use msg::Msg;
+use std::{
+    fs,
+    io::{BufReader, Write},
+};
+use xml::{reader::XmlEvent, EventReader};
+
+const API_DEV_KEY: &'static str = "Uom1Fo31YrYn0kUq2Owx1QPGRcqMq7Tp";
+
+const API_USER_NAME: &'static str = "arturhelmanau";
+
+const API_USER_PASSWORD: &'static str = "pamqad-dacgus-3Fepti";
+
+fn post(url: &str, data: String, response_capacity: usize) -> anyhow::Result<String> {
+    let mut handle = curl::Easy::new();
+    handle.url(url)?;
+    handle.post(true)?;
+    let mut written = false;
+    handle.read_function(move |mut bytes| {
+        if written {
+            Ok(0)
+        } else if bytes.write(data.as_bytes()).is_ok() {
+            written = true;
+            Ok(data.len())
+        } else {
+            Err(curl::ReadError::Abort)
+        }
+    })?;
+    let mut response_bytes = Vec::with_capacity(response_capacity);
+    {
+        let mut transfer = handle.transfer();
+
+        transfer
+            .write_function(|bytes| {
+                response_bytes.extend_from_slice(bytes);
+                Ok(bytes.len())
+            })
+            .unwrap();
+        transfer.perform()?;
+    }
+    let response = String::from_utf8(response_bytes)?;
+    if response.starts_with("Bad") {
+        anyhow::bail!("{response}");
+    } else {
+        Ok(response)
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn api_user_key() -> anyhow::Result<String> {
+    if let Ok(api_user_key_bytes) = fs::read("api_user_key.txt") {
+        return Ok(String::from_utf8(api_user_key_bytes)?);
     }
+
+    let mut data = String::with_capacity(512);
+    data.push_str("api_dev_key=");
+    data.push_str(API_DEV_KEY);
+    data.push_str("&api_user_name=");
+    data.push_str(API_USER_NAME);
+    data.push_str("&api_user_password=");
+    data.push_str(API_USER_PASSWORD);
+
+    let key = post("https://pastebin.com/api/api_login.php", data, 512)?;
+
+    fs::write("api_user_key.txt", &key)?;
+    Ok(key)
+}
+
+fn get(api_user_key: &str, api_paste_key: &str) -> anyhow::Result<String> {
+    let mut data = String::with_capacity(512);
+    data.push_str("api_dev_key=");
+    data.push_str(API_DEV_KEY);
+    data.push_str("&api_paste_key=");
+    data.push_str(api_paste_key);
+    data.push_str("&api_option=show_paste&api_user_key=");
+    data.push_str(api_user_key);
+
+    post("https://pastebin.com/api/api_post.php", data, 16384)
+}
+
+pub type ApiPasteKey = String;
+
+pub fn collect(api_user_key: &str) -> anyhow::Result<Vec<(ApiPasteKey, Msg)>> {
+    let mut data = String::with_capacity(256);
+    data.push_str("api_dev_key=");
+    data.push_str(API_DEV_KEY);
+    data.push_str("&api_option=list&api_results_limit=1000&api_user_key=");
+    data.push_str(&api_user_key);
+
+    let response = post("https://pastebin.com/api/api_post.php", data, 16384)?;
+    if response == "No pastes found." {
+        return Ok(vec![]);
+    }
+
+    let raw = BufReader::new(response.as_bytes());
+    let parser = EventReader::new(raw);
+    let mut key_paste = Vec::with_capacity(1000);
+    let mut is_key_next = false;
+    for event in parser {
+        if is_key_next {
+            let characters = match event {
+                Ok(XmlEvent::Characters(characters)) => characters,
+                _ => unreachable!(),
+            };
+            if let Ok(paste) = serde_json::from_slice(get(api_user_key, &characters)?.as_bytes()) {
+                key_paste.push((characters, paste));
+            }
+            is_key_next = false;
+        } else {
+            match event {
+                Ok(XmlEvent::StartElement { name, .. }) if name.local_name == "paste_key" => {
+                    is_key_next = true
+                }
+                Err(err) => Err(err)?,
+                _ => (),
+            }
+        }
+    }
+    Ok(key_paste)
+}
+
+pub fn remove(api_user_key: &str, api_paste_key: &str) -> anyhow::Result<()> {
+    let mut data = String::with_capacity(512);
+    data.push_str("api_dev_key=");
+    data.push_str(API_DEV_KEY);
+    data.push_str("&api_paste_key=");
+    data.push_str(api_paste_key);
+    data.push_str("&api_option=delete&api_user_key=");
+    data.push_str(&api_user_key);
+
+    post("https://pastebin.com/api/api_post.php", data, 256).map(drop)
+}
+
+pub fn insert(msg: &Msg) -> anyhow::Result<String> {
+    let bytes = serde_json::to_vec(msg)?;
+    let api_user_key = api_user_key()?;
+
+    let mut data = String::with_capacity(512);
+    data.push_str("api_dev_key=");
+    data.push_str(API_DEV_KEY);
+    data.push_str("&api_paste_code=");
+    data.push_str(&String::from_utf8(bytes)?);
+    data.push_str("&api_option=paste&api_user_key=");
+    data.push_str(&api_user_key);
+
+    post("https://pastebin.com/api/api_post.php", data, 512)
 }
